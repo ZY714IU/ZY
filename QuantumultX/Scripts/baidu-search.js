3873d1f6b566c55abec81e1338eddd4a9fe05f57
YU <7240762+ZY714IU@users.noreply.github.com>
2023-06-11 19:59:55 +0800

    更新 baidu-search.js

--- QuantumultX/Scripts/baidu-search.js
+++ QuantumultX/Scripts/baidu-search.js
@@ -4,8 +4,7 @@ ScriptName 百度搜索结果广告
 ^https?:\/\/(www|m)\.baidu\.com\/.*word=.* url script-response-body https://raw.githubusercontent.com/ZY714IU/ZY/main/QuantumultX/Scripts/baidu-search.js
 ================*/
 
-let rHead = "";
-let newStyle =
-  "";
+let rHead = "<head>";
+let newStyle = "<head><style> .wpoScript, .ec_ad_results, .ec_wise_ad, .page-copyright, .c-line-clamp1{display:none!important} </style>";
 let body = $response.body.replace(rHead, newStyle);
 $done({ body });
