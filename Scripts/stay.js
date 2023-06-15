35a5848dff30c3acafc4891e202727bfc9680b6f
YU <7240762+ZY714IU@users.noreply.github.com>
2023-06-15 22:01:21 +0800

    更新 stay.js

--- Scripts/stay.js
+++ Scripts/stay.js
@@ -1,6 +1,6 @@
 /*================
 https://github.com/ddgksf2013/Scripts/blob/master/stay.js
-^https://api\.shenyin\.name/stay-fork/browse/featured$ url script-response-body https://github.com/ZY714IU/ZY/raw/main/QuantumultX/Scripts/stay.js
+^https://api\.shenyin\.name/stay-fork/browse/featured$ url script-response-body https://github.com/ZY714IU/ZY/raw/main/Scripts/stay.js
 2022-10-17
 ================*/
 
