2f2b63a177f3fcfe638719318ceaa297db4f1cb7
YU <7240762+ZY714IU@users.noreply.github.com>
2023-06-15 21:55:53 +0800

    更新 boxjs.js

--- Scripts/boxjs.js
+++ Scripts/boxjs.js
@@ -1,7 +1,7 @@
 /*================
 https://github.com/chavyleung/scripts/blob/master/box/chavy.boxjs.js
 hostname = boxjs.com, boxjs.net, *.boxjs.com, *.boxjs.net
-^https?:\/\/(.+\.)?boxjs\.(com|net) url script-analyze-echo-response https://raw.githubusercontent.com/chavyleung/scripts/master/box/chavy.boxjs.js
+^https?://(.+\.)?boxjs\.(com|net) url script-analyze-echo-response https://raw.githubusercontent.com/chavyleung/scripts/master/box/chavy.boxjs.js
 ================*/
 
 const $ = new Env('BoxJs')
