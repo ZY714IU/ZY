9083b9c23f5db06200f72a025f47ef568c82bfbf
YU <7240762+ZY714IU@users.noreply.github.com>
2023-06-15 21:50:30 +0800

    更新 baidu-zhidao.js

--- Scripts/baidu-zhidao.js
+++ Scripts/baidu-zhidao.js
@@ -1,7 +1,7 @@
 /*================
 https://github.com/RuCu6/QuanX/blob/main/Scripts/webpage/baidu-zhidao.js
 ScriptName 百度_知道搜索广告
-^https?:\/\/zhidao\.baidu\.com\/(question|index|\?fr|\?word) url script-response-body
+^https?://zhidao\.baidu\.com/(question|index|\?(fr|word)) url script-response-body
 ================*/
 
 let rHead = "<head>";
