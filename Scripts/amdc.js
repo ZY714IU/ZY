2fcb42a45abe05e0dbd8d087dbdb3c1d29d0b8af
YU <7240762+ZY714IU@users.noreply.github.com>
2023-07-09 16:43:28 +0800

    更新 amdc.js

--- Scripts/amdc.js
+++ Scripts/amdc.js
@@ -1,7 +1,7 @@
 /*================
 https://github.com/RuCu6/QuanX/blob/main/Scripts/amdc.js
 2023-06-07 22:35
-^http?://amdc\.m\.taobao\.com/amdc/mobileDispatch url script-response-header https://raw.githubusercontent.com/ZY714IU/ZY/main/Scripts/amdc.js
+^http?://amdc.m.taobao.com/amdc/mobileDispatch url script-response-header https://raw.githubusercontent.com/ZY714IU/ZY/main/Scripts/amdc.js
 ================*/
 
 const url = $request.url;
