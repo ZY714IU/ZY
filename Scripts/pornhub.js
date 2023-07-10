3c285bd504de07917d9d47736e94bdb910cad42f
YU <7240762+ZY714IU@users.noreply.github.com>
2023-07-10 09:56:47 +0800

    更新 pornhub.js

--- Scripts/pornhub.js
+++ Scripts/pornhub.js
@@ -1,24 +1,10 @@
-[rewrite_local]
+/*========
+https://github.com/ddgksf2013/Scripts/blob/master/pornhub.js
+^https?://(cn|www).pornhub.com/_xa/ads url reject-dict
+^https?://(cn|www).pornhub.com/($|(view|video).*$) url script-response-body https://raw.githubusercontent.com/ZY714IU/ZY/main/Scripts/pornhub.js
 
-^https:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
-^https?:/\/(cn|www)\.pornhub\.com\/($|(view|video).*$) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/pornhub.js
-
-[mitm] 
-
-hostname = *.pornhub.com
-
-***********************************/
-
-
-
-
-
-
-
-
-
-
+========*/
 
 var body = $response.body
-    .replace(/<head>/, '<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ddgksf2013/Html/pornhub.css" type="text/css">');
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/ZY714IU/ZY/main/Scripts/pornhub.css" type="text/css">');
 $done({ body });
